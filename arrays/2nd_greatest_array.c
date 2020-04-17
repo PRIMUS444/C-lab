2948fec954c25d618716ec5aef0fdad2c30e80f4
PRIMUS444 <pankaj85279@gmail.com>
2020-04-17 22:58:15 +0530

    2nd greatest prog added

---
+++ arrays/2nd_greatest_array.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void clrscr()
+{
+ system("clear");
+}
+
+void main()
+{
+ int x[10],a,b;                                 //variable declaration
+ clrscr();
+ for(int i=0;i<10;i++)
+  {
+   printf("Enter a number :");
+   scanf("%d",&x[i]);
+   a=x[i];
+   if(a<x[i])
+    {
+     b=a; 
+     a=x[i];
+    }
+   }
+ printf("\nGreatest number entered is   :%d",a);
+ printf("\n2nd Greatest number enter is :%d",b);
+
+}
+
